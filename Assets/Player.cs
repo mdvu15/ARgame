05df1014eb4c5a1a104ca4f273b39d737851d68b
Minh Vu <mdvu15@earlham.edu>
2017-04-08 23:01:35 -0400

    pong game

---
+++ Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public enum ePlayer
+{
+	Front,
+	Back
+}
+
+
+public class Player : MonoBehaviour {
+	
+
+	public float speed = 15f;
+	public ePlayer player;
+
+	void FixedUpdate () 
+	{
+		float inputSpeed = 0f;
+		if (player == ePlayer.Front)
+		{
+			inputSpeed = Input.GetAxisRaw("PlayerFront");
+		}
+		else if (player == ePlayer.Back)
+		{
+			inputSpeed = Input.GetAxisRaw("PlayerBack");
+		}
+
+		transform.position += new Vector3(0f, 0f, inputSpeed * speed * Time.deltaTime);
+	}
+}
